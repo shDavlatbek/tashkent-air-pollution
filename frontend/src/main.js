51e242ec323fe84f76965e17360b6f31469bcd56
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-11-19 23:36:10 +0500

    Add tabler core/icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "@tabler/core/dist/css/tabler.min.css"
+import '@tabler/core/dist/js/tabler.min.js';
+
 createApp(App).use(router).mount("#app");
