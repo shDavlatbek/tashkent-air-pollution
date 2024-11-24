fc92dc105e40e2e073ba0c187dd464b1b1082cce
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-11-24 23:16:12 +0500

    Initial Page Header Text

---
+++ frontend/src/components/HeaderTextComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page-header d-print-none">
+    <div class="container-xl">
+      <div class="row g-2 align-items-center">
+        <div class="col">
+          <h2 class="page-title">
+            {{ title }}
+          </h2>
+        </div>
+        <div class="col-auto ms-auto d-print-none">
+          <div class="btn-list">
+            <a href="javascript:void(0)" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
+              :data-bs-target="'#'+modalId">
+              <IconPlus class="icon" stroke="2" />
+              Qo'shish
+            </a>
+            <a href="javascript:void(0)" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" 
+              :data-bs-target="'#'+modalId"
+              aria-label="Qo'shish">
+              <IconPlus class="icon" stroke="2" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { IconPlus } from '@tabler/icons-vue';
+
+
+export default {
+  name: 'HeaderText',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    modalId: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    IconPlus
+  }
+}
+
+</script>
