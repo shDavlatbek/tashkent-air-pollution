9e044d65ca943b4698630047414435042ff72270
shDavlatbek <sh.davlatbek006@gmail.com>
2024-12-03 13:55:02 +0500

    Single page init

--- frontend/src/views/GeoSingleView.vue
+++ frontend/src/views/GeoSingleView.vue
@@ -1,15 +1,128 @@
 <template>
-  <div>{{ this.$route.params.number }}</div>
+  <div class="page-body">
+    <div class="container-xl">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Gidrogeologik quduq ma'lumotlari</h3>
+        </div>
+        <div class="card-body">
+          <div class="datagrid">
+            <div class="datagrid-item">
+              <div class="datagrid-title">Registrar</div>
+              <div class="datagrid-content">Third Party</div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Nameservers</div>
+              <div class="datagrid-content">Third Party</div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Port number</div>
+              <div class="datagrid-content">3306</div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Expiration date</div>
+              <div class="datagrid-content">–</div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Creator</div>
+              <div class="datagrid-content">
+                <div class="d-flex align-items-center">
+                  <span class="avatar avatar-xs me-2 rounded"
+                    style="background-image: url(./static/avatars/000m.jpg)"></span>
+                  Paweł Kuna
+                </div>
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Age</div>
+              <div class="datagrid-content">15 days</div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Edge network</div>
+              <div class="datagrid-content">
+                <span class="status status-green">
+                  Active
+                </span>
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Avatars list</div>
+              <div class="datagrid-content">
+                <div class="avatar-list avatar-list-stacked">
+                  <span class="avatar avatar-xs rounded"
+                    style="background-image: url(./static/avatars/000m.jpg)"></span>
+                  <span class="avatar avatar-xs rounded">JL</span>
+                  <span class="avatar avatar-xs rounded"
+                    style="background-image: url(./static/avatars/002m.jpg)"></span>
+                  <span class="avatar avatar-xs rounded"
+                    style="background-image: url(./static/avatars/003m.jpg)"></span>
+                  <span class="avatar avatar-xs rounded"
+                    style="background-image: url(./static/avatars/000f.jpg)"></span>
+                  <span class="avatar avatar-xs rounded">+3</span>
+                </div>
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Checkbox</div>
+              <div class="datagrid-content">
+                <label class="form-check">
+                  <input class="form-check-input" type="checkbox" checked>
+                  <span class="form-check-label">Click me</span>
+                </label>
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Icon</div>
+              <div class="datagrid-content">
+                <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  class="icon text-green">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l5 5l10 -10" />
+                </svg>
+                Checked
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Form control</div>
+              <div class="datagrid-content">
+                <input type="text" class="form-control form-control-flush" placeholder="Input placeholder">
+              </div>
+            </div>
+            <div class="datagrid-item">
+              <div class="datagrid-title">Longer description</div>
+              <div class="datagrid-content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { getWell } from '@/api/geo';
+
+
 
 export default {
-  data() {
-    return {};
-  },
-  
+  data: () => ({
+    wellId: null
+  }),
+
   components: {
+  },
+
+  async mounted() {
+    this.wellId = this.$route?.params?.id; // Access $route in the mounted hook
+    if (this.wellId) {
+      await getWell(this.wellId);
+    } else {
+      console.error('No wellId found in route parameters.');
+    }
   },
 };
 </script>
