6eb0870c507c8f2630989a372b25493afe30c346
shDavlatbek <sh.davlatbek006@gmail.com>
2024-12-17 02:55:40 +0500

    Added caching

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,27 @@
+from typing import AsyncIterator
 import uvicorn
 from fastapi import FastAPI
 from api.routers import all_routers
 from starlette.middleware.cors import CORSMiddleware
+from fastapi_cache import FastAPICache
+# from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.backends.memcached import MemcachedBackend
+from contextlib import asynccontextmanager
+# import aioredis
 import argparse
+import aiomcache
 
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    # redis = aioredis.from_url("redis://localhost:6379")
+    FastAPICache.init(MemcachedBackend(aiomcache), prefix="fastapi-cache")
+    yield
+    
+    
 app = FastAPI(
-    title="Unit Of Work & FastAPI Users"
+    title="Unit Of Work & FastAPI Users", lifespan=lifespan
 )
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +30,7 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
+
 
 for router in all_routers:
     app.include_router(router)
